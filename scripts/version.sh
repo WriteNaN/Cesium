a35db6d6fbf255637ad7ee8f58e4866133b8aaa6
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-04-03 18:12:32 +0530

    PR handler

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,6 +5,11 @@ increment_version() {
     local version=$(echo "$body" | grep -oE '^Version:\s*(.+)$' | sed -E 's/^Version:\s*(.+)$/\1/')
     local changes=$(echo "$body" | sed -n '/# Changes/,$p' | sed '1d')
     local incremented_version=$(echo "$version" | awk -F. '{$NF++; OFS="."; print $0}')
+    
+    echo "Extracted Version: $version"
+    echo "Extracted Changes: $changes"
+    echo "Incremented Version: $incremented_version"
+    
     echo "$incremented_version"
     echo "$changes"
 }
