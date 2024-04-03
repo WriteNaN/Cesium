dd02b18895ec3e9d2fca48a50a075553ec48778e
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-04-03 18:25:47 +0530

    Mock #8

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,8 +4,8 @@ extract_version_and_changes() {
     local body=$1
     local version=$(echo "$body" | grep -oE '^Version:\s*(.+)$' | sed -E 's/^Version:\s*(.+)$/\1/')
     local changes=$(echo "$body" | sed -n '/# Changes/,$p' | sed '1d')
-    echo "$version"
-    echo "$changes"
+    echo "::set-output name=version::$version"
+    echo "::set-output name=changes::$changes"
 }
 
 extract_version_and_changes "$1"
